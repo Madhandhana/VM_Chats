aa07329583b18ccef919c599c62e4c988533f2cf
Madhandhana <dssmadhan@gmail.com>
2024-11-16 16:38:36 +0530

    with firebase

--- app/src/main/java/com/example/vmchats/login_otp.java
+++ app/src/main/java/com/example/vmchats/login_otp.java
@@ -6,6 +6,11 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class login_otp extends AppCompatActivity {
 
@@ -18,5 +23,8 @@ public class login_otp extends AppCompatActivity {
         setContentView(R.layout.activity_login_otp);
         phoneNumber = getIntent().getStringExtra("phone");
         Toast.makeText(getApplicationContext(),phoneNumber,Toast.LENGTH_LONG).show();
+
+        Map<String, String> data = new HashMap<>();
+        FirebaseFirestore.getInstance().collection("test").add(data);
     }
 }
