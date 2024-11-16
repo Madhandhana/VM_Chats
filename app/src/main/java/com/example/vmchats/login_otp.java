53f553c9a8ea6dfc81185b94747f4b592703e4f9
Stanford-mcfly <vijayganesankg@gmail.com>
2024-11-16 19:28:51 +0530

    Not Working otp

--- app/src/main/java/com/example/vmchats/login_otp.java
+++ app/src/main/java/com/example/vmchats/login_otp.java
@@ -1,30 +1,111 @@
 package com.example.vmchats;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.vmchats.utils.AndroidUtil;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 
 public class login_otp extends AppCompatActivity {
 
     String phoneNumber;
+    String verificationcode;
+    PhoneAuthProvider.ForceResendingToken resendingToken;
+    Long timeoutSeconds = 60L;
+    EditText otpinput;
+    Button nxtBtn;
+    ProgressBar progressBar;
+    TextView resendOtpTextView;
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_otp);
-        phoneNumber = getIntent().getStringExtra("phone");
-        Toast.makeText(getApplicationContext(),phoneNumber,Toast.LENGTH_LONG).show();
 
-        Map<String, String> data = new HashMap<>();
-        FirebaseFirestore.getInstance().collection("test").add(data);
+        otpinput = findViewById(R.id.otp);
+        nxtBtn = findViewById(R.id.otp_next);
+        progressBar = findViewById(R.id.progress);
+        resendOtpTextView = findViewById(R.id.resend_otp);
+        phoneNumber = getIntent().getStringExtra("phone");
+        sendOtp(phoneNumber, false);
+
     }
+
+    void sendOtp(String phoneNumber , boolean isResend) {
+        // Send OTP to the phone number
+        // You can use Firebase Auth or any other service to send OTP
+        setInProgress(true);
+        PhoneAuthOptions.Builder builder =
+                PhoneAuthOptions.newBuilder(mAuth)
+                .setPhoneNumber(phoneNumber)
+                .setTimeout(timeoutSeconds, TimeUnit.SECONDS)
+                .setActivity(this)
+                .setCallbacks(new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                    @Override
+                    public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                        signin(phoneAuthCredential);
+                        setInProgress(false);
+                    }
+
+                    @Override
+                    public void onVerificationFailed(@NonNull FirebaseException e) {
+                        AndroidUtil.showToast(getApplicationContext(),"Otp verfication Failed");
+                        setInProgress(false);
+                    }
+                    @Override
+                    public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                        super.onCodeSent(s, forceResendingToken);
+                        verificationcode = s;
+                        resendingToken = forceResendingToken;
+                        AndroidUtil.showToast(getApplicationContext(),"Otp sent successfully");
+                        setInProgress(false);
+
+                    }
+                });
+
+        if(isResend){
+            PhoneAuthProvider.verifyPhoneNumber(builder.setForceResendingToken(resendingToken).build());
+        }else{
+            PhoneAuthProvider.verifyPhoneNumber(builder.build());
+        }
+
+
+    }
+    void setInProgress(boolean inProgress){
+        if(inProgress){
+            progressBar.setVisibility(View.VISIBLE);
+            nxtBtn.setVisibility(View.GONE);
+        }
+        else{
+            progressBar.setVisibility(View.GONE);
+            nxtBtn.setVisibility(View.VISIBLE);
+        }
+    }
+
+    void signin(PhoneAuthCredential phoneAuthCredential){
+
+    }
+
 }
---
+++ app/src/main/java/com/example/vmchats/utils/AndroidUtil.java
@@ -0,0 +1,12 @@
+package com.example.vmchats.utils;
+
+import android.widget.Toast;
+import android.content.Context;
+
+public class AndroidUtil
+{
+
+    public static void showToast(Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+    }
+}
